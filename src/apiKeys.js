056e54bbce2624b50dd8d257a04e6af818e1e2ad
Srushti2404 <139379529+Srushti2404@users.noreply.github.com>
2023-10-31 19:38:46 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org  
 module.exports = {
-    key: "30aa7382dc2ec9b00ebc643c22250c40",
+    key: {" Enter your api key here"},
     base: "https://api.openweathermap.org/data/2.5/",
   };
